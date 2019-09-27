3766baa2d3b5a2e297f2250a3f18b9666c110d05
罗仁杰 <luorjna@163.com>
2019-09-27 11:09:17 +0800

    更新工具类

--- RandomCodeUtil.java
+++ RandomCodeUtil.java
@@ -1,158 +1,137 @@
 package com.casic.bonus.service.util;
 
-import java.util.HashSet;
+import java.text.DecimalFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
-import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * 随机码生成工具类
+ */
 public class RandomCodeUtil {
-    public static final String ALLCHAR = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static final String LETTERCHAR = "abcdefghijkllmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static final String NUMBERCHAR = "0123456789";
-
+    public static final String NUMBER_UPLETTER_CHAR = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     /**
-     * 返回一个定长的随机字符串(数字)
+     * 根据券码长度生成优惠券码
      *
-     * @param length 随机字符串长度
-     * @return 随机字符串
+     * @param length
+     * @return
      */
-    public static String generateOnlyNumber(int length) {
+    public static String generateCouponCode(int length) {
         StringBuffer sb = new StringBuffer();
         Random random = new Random();
         for (int i = 0; i < length; i++) {
-            sb.append(NUMBERCHAR.charAt(random.nextInt(NUMBERCHAR.length())));
-        }
-        return sb.toString();
-    }
-
-
-    /**
-     * 返回一个定长的随机字符串(只包含大小写字母、数字)
-     *
-     * @param length 随机字符串长度
-     * @return 随机字符串
-     */
-    public static String generateString(int length) {
-        StringBuffer sb = new StringBuffer();
-        Random random = new Random();
-        for (int i = 0; i < length; i++) {
-            sb.append(ALLCHAR.charAt(random.nextInt(ALLCHAR.length())));
+            sb.append(NUMBER_UPLETTER_CHAR.charAt(random.nextInt(NUMBER_UPLETTER_CHAR.length())));
         }
         return sb.toString();
     }
 
     /**
-     * 返回一个定长的随机纯字母字符串(只包含大小写字母)
+     * 生成当前大写字母（A-Z）+1的字母
      *
-     * @param length 随机字符串长度
-     * @return 随机字符串
+     * @param c
+     * @return
      */
-    public static String generateMixString(int length) {
+    public static String generateUpLetter(char c) {
+        if (c < 90) {
+            c = (char) (c + 1);
+        }
+        String s = String.valueOf(c);
+        return s;
+    }
+
+    /**
+     * 生成指定长度的随机数
+     *
+     * @param length
+     * @return
+     */
+    public static String generateRandomNumber(int length) {
+        Double bit = Math.pow(10, length);
+        Long num = Math.round(Math.random() * bit);
+        String s = String.format("%0" + length + "d", num);
+        return s;
+    }
+
+    /**
+     * 通过时间戳生成优惠券码
+     *
+     * @param time
+     */
+    public static String generateCouponCode(Instant time) {
+        Long milli = time.toEpochMilli();
+        String hexString = Long.toHexString(milli);
+        String upperCase = hexString.toUpperCase();
+        return upperCase;
+    }
+
+    /**
+     * 将字符串根据一些特殊符号分割，并将分割后的字符串首字母取出组合成新字符串
+     *
+     * @param str
+     * @param s
+     * @return
+     */
+    public static String generateFirstWordSplitChars(String str, String s) {
+        String[] ss = str.split(s);
         StringBuffer sb = new StringBuffer();
-        Random random = new Random();
-        for (int i = 0; i < length; i++) {
-            sb.append(LETTERCHAR.charAt(random.nextInt(LETTERCHAR.length())));
+        for (int i = 0; i < ss.length; i++) {
+            System.out.println(ss[i]);
+            sb.append(ss[i].charAt(0));
         }
         return sb.toString();
     }
 
     /**
-     * 返回一个定长的随机纯大写字母字符串(只包含大小写字母)
+     * 将字符串根据一些特殊符号分割,去掉首尾空字符
      *
-     * @param length 随机字符串长度
-     * @return 随机字符串
+     * @param str
+     * @param s
+     * @return
      */
-    public static String generateLowerString(int length) {
-        return generateMixString(length).toLowerCase();
-    }
-
-    /**
-     * 返回一个定长的随机纯小写字母字符串(只包含大小写字母)
-     *
-     * @param length 随机字符串长度
-     * @return 随机字符串
-     */
-    public static String generateUpperString(int length) {
-        return generateMixString(length).toUpperCase();
-    }
-
-    /**
-     * 生成一个定长的纯0字符串
-     *
-     * @param length 字符串长度
-     * @return 纯0字符串
-     */
-    public static String generateZeroString(int length) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < length; i++) {
-            sb.append('0');
+    public static List getWordsSplitChars(String str, String s) {
+        String[] ss = str.split(s);
+        List<String> strList = new ArrayList();
+        for (int i = 0; i < ss.length; i++) {
+            strList.add(ss[i].trim());
         }
-        return sb.toString();
+        return strList;
     }
 
     /**
-     * 根据数字生成一个定长的字符串，长度不够前面补0
+     * 含有数字的字符串保留数字
      *
-     * @param num       数字
-     * @param fixdlenth 字符串长度
-     * @return 定长的字符串
+     * @param a
+     * @return
      */
-    public static String toFixdLengthString(long num, int fixdlenth) {
-        StringBuffer sb = new StringBuffer();
-        String strNum = String.valueOf(num);
-        if (fixdlenth - strNum.length() >= 0) {
-            sb.append(generateZeroString(fixdlenth - strNum.length()));
+    public static Integer getNumberFromChars(String a) {
+        String regEx = "[^0-9]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(a);
+        String s = m.replaceAll("").trim();
+        int num = Integer.parseInt(s);
+        return num;
+    }
+
+    /**
+     * 截取前/后指定位数的字符
+     * @param str
+     * @param len 截取长度
+     * @param isEndChars （true：后{len}位； false：前{len}位）
+     * @return
+     */
+    public static String getSubStringChars(String str, Integer len, boolean isEndChars) {
+        String s;
+        if (isEndChars) {
+            s = str.substring(str.length() - len);
         } else {
-            throw new RuntimeException("将数字" + num + "转化为长度为" + fixdlenth
-                + "的字符串发生异常！");
+            s = str.substring(0, len);
         }
-        sb.append(strNum);
-        return sb.toString();
-    }
 
-    /**
-     * 每次生成的len位数都不相同
-     *
-     * @param param
-     * @return 定长的数字
-     */
-    public static int getNotSimple(int[] param, int len) {
-        Random rand = new Random();
-        for (int i = param.length; i > 1; i--) {
-            int index = rand.nextInt(i);
-            int tmp = param[index];
-            param[index] = param[i - 1];
-            param[i - 1] = tmp;
-        }
-        int result = 0;
-        for (int i = 0; i < len; i++) {
-            result = result * 10 + param[i];
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("返回一个定长的随机字符串(只包含大小写字母、数字):" + generateString(10));
-        System.out
-            .println("返回一个定长的随机纯字母字符串(只包含大小写字母):" + generateMixString(10));
-        System.out.println("返回一个定长的随机纯小写字母字符串(只包含大小写字母):"
-            + generateLowerString(10));
-        System.out.println("返回一个定长的随机纯大写字母字符串(只包含大小写字母):"
-            + generateUpperString(10));
-        System.out.println("生成一个定长的纯0字符串:" + generateZeroString(10));
-        System.out.println("根据数字生成一个定长的字符串，长度不够前面补0:"
-            + toFixdLengthString(123, 10));
-        int[] in = {1, 2, 3, 4, 5, 6, 7};
-        System.out.println("每次生成的len位数都不相同:" + getNotSimple(in, 3));
-
-        int i = 0;
-        Set codes = new HashSet();
-        while (codes.size() < 10) {
-            i++;
-            codes.add(generateOnlyNumber(10));
-
-        }
-        System.out.println("返回一个定长的随机字符串(数字):" + codes.toString());
-        System.out.println("返回一个定长的随机字符串(数字) 产生次数:" + i);
+        return s;
     }
 }
